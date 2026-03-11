ed9b0e37be44f4918689c1d6c45b8d18a620e43d
Diogo Nunes <diogo@colsaofrancisco.com.br>
2026-03-11 11:27:46 -0300

    atualização de formatação

--- group/sync-group-members.ps1
+++ group/sync-group-members.ps1
@@ -13,13 +13,13 @@
     Última atualização: 03/04/2025
 #>
 
-﻿$grupos = Import-Csv "D:\Downloads\sync.csv"
+$grupos = Import-Csv "D:\Downloads\sync.csv"
 
 foreach ($grupo in $grupos) {
 
-$novo = $grupo.grupo
-$sync = $grupo.sync
+  $novo = $grupo.grupo
+  $sync = $grupo.sync
 
-gam update group $novo sync member group $sync
+  gam update group $novo sync member group $sync
 
 }
